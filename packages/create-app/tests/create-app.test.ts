d6336406dc5076f646d893c13ec0f3d7445b5ea2
Marcio Zebedeu <marciozebedeu@gmail.com>
2026-08-05 16:21:15 +0100

    chore: rename scaffolder create-machize-app -> create-mach
    Shorter, consistent with the mach CLI: npx create-mach my-saas.

--- packages/create-app/tests/create-app.test.ts
+++ packages/create-app/tests/create-app.test.ts
@@ -7,7 +7,7 @@ import { createProject, TargetNotEmptyError } from '../src/index.js'
 let root: string
 
 beforeEach(async () => {
-  root = await mkdtemp(join(tmpdir(), 'create-machize-app-'))
+  root = await mkdtemp(join(tmpdir(), 'create-mach-'))
 })
 
 afterEach(async () => {
